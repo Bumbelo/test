fd7de839df7a6d6b31ae02ce58eb167e873df11b
Piotr Wyrostek <bumbelopl@gmail.com>
2017-04-22 17:44:39 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+/*global $ */
+$('#wrapper').hide();
+function main() {
+    $('#wrapper').fadeIn(500);
+    
+    $('.shopping-list').hide();
+    $('.shopping-button').on('click', function(){
+        $(this).next().slideToggle(400);
+        $(this).toggleClass('activebutton');
+        if ($(this).text()==='Zobacz'){
+            $(this).text('Ukryj');
+        }
+        else{
+            $(this).text('Zobacz');
+        }
+    });
+}
+$(document).ready(main);
